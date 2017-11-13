1f69a14b668d1be5f8680f0f1914c6a9b2dddee2
Damien Carver <carverdamien@gmail.com>
2017-11-13 16:48:22 +0100

    fix

--- lab/00/run.sh
+++ lab/00/run.sh
@@ -12,7 +12,7 @@ docker-compose -f unrestricted.yml up -d
 docker-compose -f unrestricted.yml exec sysbencha prepare --dbsize ${DBSIZE}
 docker-compose -f unrestricted.yml exec sysbenchb prepare --dbsize ${DBSIZE}
 docker-compose -f unrestricted.yml exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
-docker-compose -f unrestricted.yml exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/memory/consolidate ] || find /rootfs/sys/fs/cgroup/memory/consolidate -type d -exec rmdir {} \;'
+docker-compose -f unrestricted.yml exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/memory/consolidate ] || rmdir /rootfs/sys/fs/cgroup/memory/consolidate'
 docker-compose -f unrestricted.yml exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/memory/consolidate'
 docker-compose -f unrestricted.yml exec host bash -c 'echo 1 > /rootfs/sys/fs/cgroup/memory/consolidate/memory.use_hierarchy'
 docker-compose -f unrestricted.yml exec host bash -c 'echo ${MEMORY} > /rootfs/sys/fs/cgroup/memory/consolidate/memory.limit_in_bytes'
