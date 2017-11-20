1b5d218994dab2d8f1f08f6b6107bb528336140f
Damien Carver <carverdamien@gmail.com>
2017-11-20 15:52:45 +0100

    comments

--- doc/mark_page_accessed.c
+++ doc/mark_page_accessed.c
@@ -1,3 +1,13 @@
+/*
+ * Mark a page as having seen activity.
+ *
+ * inactive,unreferenced	->	inactive,referenced
+ * inactive,referenced		->	active,unreferenced
+ * active,unreferenced		->	active,referenced
+ *
+ * When a newly allocated page is not yet visible, so safe for non-atomic ops,
+ * __SetPageReferenced(page) may be substituted for mark_page_accessed(page).
+ */
 void mark_page_accessed(struct page *page)
 {
 	page = compound_head(page);
