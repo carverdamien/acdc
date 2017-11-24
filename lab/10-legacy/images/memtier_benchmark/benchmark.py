6eb301589f7f20c696437faf4c3747895b8599a0
Damien Carver <carverdamien@gmail.com>
2017-11-24 16:17:17 +0100

    memtier like sysbench

---
+++ lab/10-legacy/images/memtier_benchmark/benchmark.py
@@ -0,0 +1,48 @@
+import subprocess
+import time
+import argparse
+import parse
+import influxdb
+import datetime
+
+def run(args):
+    client = influxdb.InfluxDBClient(host=args.influxdbhost,
+                                    port=int(args.influxdbport),
+                                    database=args.influxdbname)
+    client.create_database(args.influxdbname)
+    measurement = 'memtier_stats'
+    tags = {}
+    def callback(fields):
+        client.write_points([p for p in influxformat(measurement, fields, tags=tags)])
+
+    call = args.call
+    p = subprocess.Popen(call, stdout=subprocess.PIPE)
+    for line in p.stdout:
+        print(line)
+
+def influxformat(measurement, fields, tags={}):
+    t = datetime.datetime.utcfromtimestamp(int(fields['timestamp']))
+    del fields['timestamp']
+    point = {
+        "measurement": measurement,
+        "tags": tags,
+        "time": t,
+        "fields": { k:float(fields[k]) for k in fields},
+    }
+    yield point
+
+def main():
+    main_parser = argparse.ArgumentParser()
+    main_subparsers = main_parser.add_subparsers()
+
+    run_parser = main_subparsers.add_parser('run')
+    run_parser.add_argument("--influxdbname", dest="influxdbname", type=str, nargs=1, default='acdc')
+    run_parser.add_argument("--influxdbhost", dest="influxdbhost", type=str, nargs=1, default='influxdb')
+    run_parser.add_argument("--influxdbport", dest="influxdbport", type=str, nargs=1, default='8086')
+    run_parser.add_argument('call', metavar='N', type=str, nargs='+')
+
+    args = main_parser.parse_args()
+    
+    print(args)
+
+main()
