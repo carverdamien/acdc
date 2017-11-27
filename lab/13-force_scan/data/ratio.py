fa0a82053d5ed14b9c2230de61a990087e64954e
Damien Carver <carverdamien@gmail.com>
2017-11-27 12:00:19 +0100

    init 13-force_scan/ 14-idle_page_tracking/

---
+++ lab/13-force_scan/data/ratio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pandas as pd
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+img = sys.argv[1]
+
+df = pd.read_csv('memory_stats.csv')
+
+plt.figure()
+for label in ['mysqla', 'mysqlb']:
+	sel = df['com.docker.compose.service'] == label
+	X = df['time'][sel]
+	X = np.array(X, dtype='datetime64[ns]')
+	Y = df['stats.recent_ratio_total'][sel]
+	plt.plot(X,Y,label=label)
+plt.legend()
+plt.savefig(img)
