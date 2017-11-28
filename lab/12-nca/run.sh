08223a4bf4576dbff028cd5000afa8f12a7fc210
Damien Carver <carverdamien@gmail.com>
2017-11-28 15:14:42 +0100

    update 12 from 14

--- lab/12-nca/run.sh
+++ lab/12-nca/run.sh
@@ -5,7 +5,7 @@ source kernel
 [ -n "${KERNEL}" ]
 [ "$(uname -sr)" == "Linux ${KERNEL}" ]
 
-: ${MEMORY:=4874100736}
+: ${MEMORY:=$((2*2*2**30+640*2**20))}
 
 SIZE=$((2**12)) # 512MB max
 REQUESTS=$((2*189841)) #$((2**30/${SIZE}/2))
@@ -22,6 +22,7 @@ ${PRE} create
 ${PRE} up -d
 #${PRE} exec memtiera run -- memtier_benchmark -s redisa ${EXTRA_INIT} #debug
 #${PRE} exec memtierb run -- memtier_benchmark -s redisb ${EXTRA_INIT} #debug
+#${PRE} exec memtierc run -- memtier_benchmark -s redisc ${EXTRA_INIT} #debug
 ${PRE} exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
 ${PRE} exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/memory/consolidate ] || rmdir /rootfs/sys/fs/cgroup/memory/consolidate'
 ${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/memory/consolidate'
@@ -31,27 +32,39 @@ ${PRE} exec host bash -c "echo ${MEMORY} > /rootfs/sys/fs/cgroup/memory/consolid
 ${PRE} down
 
 EXTRA_HIGH="-d ${SIZE} --key-pattern=R:R --key-maximum=${REQUESTS} --ratio=0:1 -c 1 -t 1"
-EXTRA_LOW="-d ${SIZE} --key-pattern=R:R --key-maximum=$((REQUESTS * 20 / 100)) --ratio=0:1 -c 1 -t 1"
+EXTRA_LOW="-d ${SIZE} --key-pattern=R:R --key-maximum=$((REQUESTS * 15 / 100)) --ratio=0:1 -c 1 -t 1"
 
 # Run
 ${RUN} create
 ${RUN} up -d
+
+${RUN} exec redisc redis-cli config set maxmemory 2gb
+${RUN} exec redisc redis-cli config set maxmemory-policy allkeys-lru
+${RUN} exec redisc redis-cli config set appendonly no
+${RUN} exec memtierc run -- memtier_benchmark -s redisc ${EXTRA_INIT}
+${RUN} exec redisc redis-cli save
+
 ${RUN} exec redisa redis-cli config set maxmemory 2gb
 ${RUN} exec redisa redis-cli config set maxmemory-policy allkeys-lru
 ${RUN} exec redisa redis-cli config set appendonly no
+${RUN} exec memtiera run -- memtier_benchmark -s redisa ${EXTRA_INIT}
+${RUN} exec redisa redis-cli save
+
 ${RUN} exec redisb redis-cli config set maxmemory 2gb
 ${RUN} exec redisb redis-cli config set maxmemory-policy allkeys-lru
 ${RUN} exec redisb redis-cli config set appendonly no
-${RUN} exec memtiera run -- memtier_benchmark -s redisa ${EXTRA_INIT}
 ${RUN} exec memtierb run -- memtier_benchmark -s redisb ${EXTRA_INIT}
+${RUN} exec redisb redis-cli save
+
 ${RUN} exec memtiera job run -- memtier_benchmark -s redisa ${EXTRA_HIGH} --test-time 300
 ${RUN} exec memtierb job run -- memtier_benchmark -s redisb ${EXTRA_HIGH} --test-time 60
+${RUN} exec memtierc job run -- memtier_benchmark -s redisc ${EXTRA_LOW} --test-time 120
 sleep 60
 ${RUN} exec memtierb job run -- memtier_benchmark -s redisb ${EXTRA_LOW} --test-time 180
 sleep 60
-${RUN} exec cassandra job start
+${RUN} exec memtierc job run -- memtier_benchmark -s redisc ${EXTRA_HIGH} --test-time 60
 sleep 60
-${RUN} exec cassandra job stop
+${RUN} exec memtierc job run -- memtier_benchmark -s redisc ${EXTRA_LOW} --test-time 120
 sleep 60
 ${RUN} exec memtierb job run -- memtier_benchmark -s redisb ${EXTRA_HIGH} --test-time 60
 sleep 60
