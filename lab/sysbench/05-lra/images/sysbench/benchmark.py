d85ff979001c9cf9d2b9968d4a8ae1315003991b
Damien Carver <carverdamien@gmail.com>
2017-11-30 13:02:37 +0100

    cp -a 02-nca 05-lra

---
+++ lab/sysbench/05-lra/images/sysbench/benchmark.py
@@ -0,0 +1,118 @@
+import subprocess
+import time
+import argparse
+import parse
+import influxdb
+import datetime
+
+main_parser = argparse.ArgumentParser()
+# TODO
+user = 'root'
+password = ''
+host = 'mysql'
+dbname = 'dbname'
+main_subparsers = main_parser.add_subparsers()
+
+sysbench_bin_path = './sysbench/sysbench'
+sysbench_lua_path = './sysbench/tests/db/oltp.lua'
+
+mysql_call = ['mysql',
+              '--host', host,
+              '-u', user]
+
+def sysbench_call(dbsize):
+    return [sysbench_bin_path,
+            '--test=%s' % sysbench_lua_path,
+            '--oltp-table-size=%d' % dbsize,
+            '--mysql-db=%s' % dbname,
+            '--mysql-host=%s' % host,
+            '--mysql-user=%s' % user,
+            '--mysql-password=%s' % password]
+
+sysbench_expected_v05_intermediate_output = """[{}] timestamp: {timestamp}, threads: {threads}, tps: {trps}, reads: {rdps}, writes: {wrps}, response time: {rtps}ms ({}%), errors: {errps}, reconnects:  {recops}"""
+sysbenchoutput_parser = parse.compile(sysbench_expected_v05_intermediate_output)
+
+def wait_for_server_to_start():
+    while True:
+        try:
+            subprocess.check_call(mysql_call + ['-e', 'show status'])
+            break
+        except Exception as e:
+            print(e)
+        print('Waiting for %s to start' % (host))
+        time.sleep(1)
+        
+def prepare(args):
+    try:
+        subprocess.check_call(mysql_call + ['-e', 'CREATE DATABASE %s' % dbname])
+        subprocess.check_call(sysbench_call(args.dbsize) + ['prepare'])
+    except Exception as e:
+        print(e)
+    finally:
+        out = subprocess.check_output(mysql_call + ['-D', dbname, '-e', 'select count(*) from sbtest1'])
+        count = int(out.split('\n')[1])
+        if count != args.dbsize:
+            raise Exception('count != dbsize')
+        #subprocess.check_call(mysql_call + ['-e', 'shutdown'])
+
+def run(args):
+    client = influxdb.InfluxDBClient(host=args.influxdbhost,
+                                    port=int(args.influxdbport),
+                                    database=args.influxdbname)
+    client.create_database(args.influxdbname)
+    measurement = 'sysbench_stats'
+    tags = {
+        'hostname' : subprocess.check_output(mysql_call + ['-BNe', 'select @@hostname;'])[:-1]
+    }
+    def callback(fields):
+        client.write_points([p for p in influxformat(measurement, fields, tags=tags)])
+    args.callback = callback
+
+    call = sysbench_call(args.dbsize) + ['--report-interval=1',
+                            '--tx-rate=%d' % 0,
+                            '--max-requests=0',
+                            '--max-time=%d' % args.duration,
+                            '--num-threads=%d' % 8,
+                            '--oltp-read-only=on',
+                            'run']
+    p = subprocess.Popen(call, stdout=subprocess.PIPE)
+    for line in p.stdout:
+        res = sysbenchoutput_parser.search(line)
+        if res == None:
+            print(line)
+        else:
+            args.callback(dict(res.named))
+
+def dummy(*args,**kwargs):
+    pass
+
+def influxformat(measurement, fields, tags={}):
+    t = datetime.datetime.utcfromtimestamp(int(fields['timestamp']))
+    del fields['timestamp']
+    point = {
+        "measurement": measurement,
+        "tags": tags,
+        "time": t,
+        "fields": { k:float(fields[k]) for k in fields},
+    }
+    yield point
+
+def main():
+    prepare_parser = main_subparsers.add_parser('prepare')
+    prepare_parser.add_argument('--dbsize',   dest="dbsize", type=int, nargs='?', default=10000)
+    prepare_parser.set_defaults(func=prepare)
+    run_parser = main_subparsers.add_parser('run')
+    run_parser.add_argument("--influxdbname", dest="influxdbname", type=str, nargs=1, default='acdc')
+    run_parser.add_argument("--influxdbhost", dest="influxdbhost", type=str, nargs=1, default='influxdb')
+    run_parser.add_argument("--influxdbport", dest="influxdbport", type=str, nargs=1, default='8086')
+    run_parser.add_argument('--dbsize',   dest="dbsize", type=int, nargs='?', default=10000)
+    run_parser.add_argument('--duration', dest="duration", type=int, nargs='?', default=60)
+    run_parser.set_defaults(func=run)
+    run_parser.set_defaults(callback=dummy)
+
+    args = main_parser.parse_args()
+    
+    wait_for_server_to_start()
+    args.func(args)
+
+main()
