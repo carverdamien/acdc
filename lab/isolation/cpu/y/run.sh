c3a1945c5a76f2fd58f5188c47af8d0830752a65
Damien Carver <carverdamien@gmail.com>
2018-02-07 17:19:59 +0100

    y

---
+++ lab/isolation/cpu/y/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -x -e
+
+source kernel
+[ -n "${KERNEL}" ]
+[ "$(uname -sr)" == "Linux ${KERNEL}" ]
+
+: ${DBSIZE:=10000000} # Use small value for debug
+: ${MEMORY:=3800358912}
+
+RUN="docker-compose --project-directory $PWD -f compose/restricted.yml"
+PRE="docker-compose --project-directory $PWD -f compose/unrestricted.yml"
+
+# Prepare
+${RUN} down
+${PRE} down
+${PRE} build
+${PRE} create
+${PRE} up -d
+${PRE} exec sysbencha prepare --dbsize ${DBSIZE}
+${PRE} exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
+${PRE} exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/memory/consolidate ] || rmdir /rootfs/sys/fs/cgroup/memory/consolidate'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/memory/consolidate'
+${PRE} exec host bash -c 'echo 1 > /rootfs/sys/fs/cgroup/memory/consolidate/memory.use_hierarchy'
+${PRE} exec host bash -c "echo ${MEMORY} > /rootfs/sys/fs/cgroup/memory/consolidate/memory.limit_in_bytes"
+${PRE} down
+
+# Run
+${RUN} create
+${RUN} up -d
+${RUN} exec sysbencha job run --dbsize ${DBSIZE} --duration 180 --tx-rate 1000
+sleep 60
+${RUN} exec sysbenchb job ./sysbench/sysbench --test=cpu --cpu-max-prime=20000 run
+sleep 120
+
+# Report
+for m in memory_stats blkio_stats networks cpu_stats sysbench_stats
+do
+	${PRE} exec influxdb influx -database acdc -execute "select * from $m" -format=csv > data/$m.csv
+done
