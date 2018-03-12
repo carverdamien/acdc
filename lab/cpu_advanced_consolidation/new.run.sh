4262c076d3529afe5eca176e1e2e4d6d297a20f1
Damien Carver <carverdamien@gmail.com>
2018-03-12 16:55:55 +0100

    new.run.sh

---
+++ lab/cpu_advanced_consolidation/new.run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -x -e
+
+source kernel
+[ -n "${KERNEL}" ]
+[ "$(uname -sr)" == "Linux ${KERNEL}" ]
+
+: ${DBSIZE:=10000000} # Use small value for debug
+: ${MEMORY:=3800358912}
+
+RUN="docker-compose --project-directory $PWD -f compose/restricted.yml"
+PRE="docker-compose --project-directory $PWD -f compose/unrestricted.yml"
+
+# Prepare
+${RUN} down
+${PRE} down
+${PRE} build
+${PRE} create
+${PRE} up -d
+${PRE} exec sysbencha prepare --dbsize ${DBSIZE}
+${PRE} exec sysbenchb prepare --dbsize ${DBSIZE}
+${PRE} exec sysbenchc prepare --dbsize ${DBSIZE}
+# ${PRE} exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
+${PRE} exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/cpu/consolidate ] || find /rootfs/sys/fs/cgroup/cpu/consolidate -type d -delete'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/A'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/BC'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/BC/B'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/BC/C'
+${PRE} exec host bash -c 'echo 1000000 > /rootfs/sys/fs/cgroup/cpu/consolidate/cpu.cfs_period_us'
+${PRE} exec host bash -c 'echo 4000000 > /rootfs/sys/fs/cgroup/cpu/consolidate/cpu.cfs_quota_us'
+${PRE} exec host bash -c 'echo 1024 > /rootfs/sys/fs/cgroup/cpu/consolidate/A/cpu.shares'
+${PRE} exec host bash -c 'echo 2 > /rootfs/sys/fs/cgroup/cpu/consolidate/BC/cpu.shares'
+${PRE} exec host bash -c 'echo 1024 > /rootfs/sys/fs/cgroup/cpu/consolidate/BC/B/cpu.shares'
+${PRE} exec host bash -c 'echo 2 > /rootfs/sys/fs/cgroup/cpu/consolidate/BC/C/cpu.shares'
+${PRE} down
+
+MAXTXR=1800 #
+CYCLE=60
+
+# Run
+${RUN} create
+${RUN} up -d
+
+GO() {
+	TXRATE=$((MAXTXR * X / 10))
+	NREQ=$((TXRATE * CYCLE))
+	${RUN} exec sysbencha run --dbsize ${DBSIZE} --tx-rate ${TXRATE} --max-requests ${NREQ}	
+}
+A() { for X in 1 1 2 4 8 4 2 1 1; do GO; done; }
+B() { for X in 2 4 8 4 2 1 1 1 1; do GO; done; }
+C() { for X in 1 1 1 1 2 4 8 4 2; do GO; done; }
+
+A&
+B&
+C&
+
+wait
+wait
+wait
+
+# Report
+for m in memory_stats blkio_stats networks cpu_stats sysbench_stats
+do
+	${PRE} exec influxdb influx -database acdc -execute "select * from $m" -format=csv > data/$m.csv
+done
