104bc5cfe13fe7f43f00b3a930ccc9650d17f5c7
Damien Carver <carverdamien@gmail.com>
2018-05-03 17:48:46 +0200

    skip init error

--- lab/explain_isolation_consolidation/run.sh
+++ lab/explain_isolation_consolidation/run.sh
@@ -41,7 +41,7 @@ esac
 start_mysqlb() {
 case ${MODE} in
 	process)
-${PRE} exec mysqla gosu mysql mysqld --initialize-insecure --datadir=/var/lib/3307.data
+${PRE} exec mysqla gosu mysql mysqld --initialize-insecure --datadir=/var/lib/3307.data || true
 ${PRE} exec mysqla gosu mysql mysqld --datadir=/var/lib/3307.data --socket=/var/run/mysqld/3307.sock --port=3307
 ;;
 esac
