8353fbd9b1d45519e44c88b2880db25d8e6408c9
Damien Carver <carverdamien@gmail.com>
2018-05-03 09:04:26 +0200

    fix

--- lab/blk_advanced_consolidation/run.sh
+++ lab/blk_advanced_consolidation/run.sh
@@ -21,18 +21,16 @@ ${PRE} exec sysbencha prepare --dbsize ${DBSIZE}
 ${PRE} exec sysbenchb prepare --dbsize ${DBSIZE}
 ${PRE} exec sysbenchc prepare --dbsize ${DBSIZE}
 # ${PRE} exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
-${PRE} exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/cpu/consolidate ] || find /rootfs/sys/fs/cgroup/cpu/consolidate -type d -delete'
-${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate'
-${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/A'
-${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/BC'
-${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/BC/B'
-${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/cpu/consolidate/BC/C'
-${PRE} exec host bash -c 'echo 1000000 > /rootfs/sys/fs/cgroup/cpu/consolidate/cpu.cfs_period_us'
-${PRE} exec host bash -c 'echo 2000000 > /rootfs/sys/fs/cgroup/cpu/consolidate/cpu.cfs_quota_us'
-${PRE} exec host bash -c 'echo 1024 > /rootfs/sys/fs/cgroup/cpu/consolidate/A/cpu.shares'
-${PRE} exec host bash -c 'echo 2 > /rootfs/sys/fs/cgroup/cpu/consolidate/BC/cpu.shares'
-${PRE} exec host bash -c 'echo 1024 > /rootfs/sys/fs/cgroup/cpu/consolidate/BC/B/cpu.shares'
-${PRE} exec host bash -c 'echo 1024 > /rootfs/sys/fs/cgroup/cpu/consolidate/BC/C/cpu.shares'
+${PRE} exec host bash -c 'echo cfq | tee /sys/block/sda/queue/scheduler'
+${PRE} exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/blkio/consolidate ] || find /rootfs/sys/fs/cgroup/blkio/consolidate -type d -delete'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/blkio/consolidate'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/blkio/consolidate/B'
+${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/blkio/consolidate/C'
+${PRE} exec host bash -c 'echo $(cat /sys/block/sda/dev) $((2**20)) > /rootfs/sys/fs/cgroup/blkio/consolidate/blkio.throttle.read_bps_device'
+${PRE} exec host bash -c 'echo 1000 > /rootfs/sys/fs/cgroup/blkio/consolidate/blkio.weight'
+${PRE} exec host bash -c 'echo 1000 > /rootfs/sys/fs/cgroup/blkio/consolidate/blkio.leaf_weight'
+${PRE} exec host bash -c 'echo 10 > /rootfs/sys/fs/cgroup/blkio/consolidate/B/blkio.weight'
+${PRE} exec host bash -c 'echo 10 > /rootfs/sys/fs/cgroup/blkio/consolidate/C/blkio.weight'
 ${PRE} down
 
 MAXTXR=870
@@ -70,6 +68,11 @@ SCHED() {
 A() { ${RUN} exec -T sysbencha python benchmark.py --wait=0 run --dbsize ${DBSIZE} $(SCHED 25 50 25 25 100 25 25 25 25 25 50 25 25 25 50 25 50 25 25 25);}
 B() { ${RUN} exec -T sysbenchb python benchmark.py --wait=0 run --dbsize ${DBSIZE} $(SCHED 25 25 50 25 25 25 100 25 25 25 50 25 50 25 25 25 50 25 25 25);}
 C() { ${RUN} exec -T sysbenchc python benchmark.py --wait=0 run --dbsize ${DBSIZE} $(SCHED 25 25 25 50 25 25 25 25 100 25 25 25 50 25 50 25 50 25 25 25);}
+
+
+A() { ${RUN} exec -T sysbencha python benchmark.py --wait=0 run --dbsize ${DBSIZE} --tx-rate=0 --duration=60; }
+B() { ${RUN} exec -T sysbenchb python benchmark.py --wait=0 run --dbsize ${DBSIZE} --tx-rate=0 --duration=60; }
+C() { ${RUN} exec -T sysbenchc python benchmark.py --wait=0 run --dbsize ${DBSIZE} --tx-rate=0 --duration=60; }
 
 A | tee A.out &
 B | tee B.out &
