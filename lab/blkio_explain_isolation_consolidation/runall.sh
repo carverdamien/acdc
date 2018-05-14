19824e723f16d6d7687594fa4dca409f3aa925be
Damien Carver <carverdamien@gmail.com>
2018-05-14 11:57:14 +0200

    limit memory to put contention on blkio

--- lab/blkio_explain_isolation_consolidation/runall.sh
+++ lab/blkio_explain_isolation_consolidation/runall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export MODE
-for MODE in Aonly Bonly isolated noshares not_isolated process standalone
+# for MODE in Aonly Bonly isolated noshares not_isolated process standalone
+# for MODE in Aonly Bonly not_isolated isolated
+for MODE in Aonly Bonly
 do
 	bash run.sh
 done
