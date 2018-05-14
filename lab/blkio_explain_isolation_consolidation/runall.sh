27b4bf271a95d16086950760dbdedaefcec4a5a2
Damien Carver <carverdamien@gmail.com>
2018-05-14 14:47:04 +0200

    process

--- lab/blkio_explain_isolation_consolidation/runall.sh
+++ lab/blkio_explain_isolation_consolidation/runall.sh
@@ -2,7 +2,7 @@
 export MODE
 # for MODE in Aonly Bonly isolated noshares not_isolated process standalone
 # for MODE in Aonly Bonly not_isolated isolated
-for MODE in Aonly Bonly not_isolated isolated
+for MODE in process Aonly Bonly isolated
 do
 	bash run.sh
 done
