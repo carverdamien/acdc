96a56aab1b0a08d03d1c12413b3081cbc8abb8a4
Damien Carver <carverdamien@gmail.com>
2018-05-28 14:12:16 +0200

    cp -a rr/cache cpu

---
+++ lab/eval/cpu/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -x -e
+
+source kernel
+[ -n "${KERNEL}" ]
+[ "$(uname -sr)" == "Linux ${KERNEL}" ]
+
+PRE="docker-compose --project-directory $PWD -f compose/unrestricted.yml"
+RUN="docker-compose --project-directory $PWD -f compose/restricted.yml"
+
+# Prepare
+make -C workloads
+${RUN} down --remove-orphans
+${PRE} down --remove-orphans
+${PRE} build
+${PRE} create
+${PRE} up -d
+${PRE} exec filebench filebench -f workloads/prepare.f
+${PRE} exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
+${PRE} exec host bash -c 'echo cfq > /sys/block/sda/queue/scheduler'
+${PRE} down
+
+# Run
+${RUN} create
+${RUN} up -d
+
+RUN() { ${RUN} exec -T filebench python benchmark.py -- filebench -f workloads/run.f;}
+
+RUN &
+
+wait
+
+# Report
+mkdir -p data
+for m in memory_stats blkio_stats networks cpu_stats filebench_stats
+do
+	${PRE} exec influxdb influx -database acdc -execute "select * from $m" -format=csv > data/$m.csv
+done
