6456018f317d8c7f74fc61c88e4b049ed51f5093
Damien Carver <carverdamien@gmail.com>
2018-05-29 09:09:55 +0200

    cp -a fn fp

---
+++ lab/eval/rrfs/fp/workloads/prepare.sh
@@ -0,0 +1,18 @@
+echo '
+set $dir=/data/A/
+set $filesize=2g
+set $iosize=1m
+
+define file name=largefile,path=$dir,size=$filesize,prealloc,reuse
+
+define process name=filereader,instances=1
+{
+  thread name=hot,memsize=2m,instances=100
+  {
+    flowop eventlimit name=limit
+    flowop read name=randread,filename=largefile,iosize=$iosize,random,workingset=1g
+  }
+}
+
+create files
+'
