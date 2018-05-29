183c1dbcc844514a36ec657a3e59e09a305a7d51
Damien Carver <carverdamien@gmail.com>
2018-05-29 09:39:45 +0200

    instances=1

--- lab/eval/rrfs/fn/workloads/prepare.sh
+++ lab/eval/rrfs/fn/workloads/prepare.sh
@@ -7,7 +7,7 @@ define file name=largefile,path=$dir,size=$filesize,prealloc,reuse
 
 define process name=filereader,instances=1
 {
-  thread name=hot,memsize=2m,instances=100
+  thread name=hot,memsize=2m,instances=1
   {
     flowop eventlimit name=limit
     flowop read name=randread,filename=largefile,iosize=$iosize,random,workingset=1g
--- lab/eval/rrfs/fp/workloads/prepare.sh
+++ lab/eval/rrfs/fp/workloads/prepare.sh
@@ -7,11 +7,11 @@ define file name=largefile,path=$dir,size=$filesize,prealloc,reuse
 
 define process name=filereader,instances=1
 {
-  thread name=hot,memsize=2m,instances=100
+  thread name=hot,memsize=2m,instances=1
   {
     flowop read name=randread,filename=largefile,iosize=$iosize,random,workingset=768m
   }
-  thread name=cold,memsize=2m,instances=100
+  thread name=cold,memsize=2m,instances=1
   {
     flowop eventlimit name=limit
     flowop read name=seqread,filename=largefile,iosize=$iosize
