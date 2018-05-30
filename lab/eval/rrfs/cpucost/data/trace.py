5a07105e9b86b8cb10ae92a2bc15cd63d067316d
Damien Carver <carverdamien@gmail.com>
2018-05-30 10:41:10 +0200

    trace.py

--- lab/eval/rrfs/cpucost/data/trace.py
+++ lab/eval/rrfs/cpucost/data/trace.py
@@ -8,26 +8,37 @@ import sys
 
 img = sys.argv[1]
 
-labels = ['0.001', '0.01', '0.1', '1']
+labels = [
+        0.001,
+        0.01,
+        0.1,
+        1,
+]
 
-def process(df):
+def process(df,N):
+        N = 1
         df['time'] = df['time'] - df['time'][0]
-        X = np.arange(max(df['time']))
-        Y = np.zeros(len(X))
-        for i in range(len(X)-1):
-                xmin = X[i]
-                xmax = X[i+1]
+        XMAX = max(df['time'])
+        Y = np.zeros(int(XMAX/N))
+        X = np.zeros(int(XMAX/N))
+        xmin = 0
+        xmax = N
+        for i in range(int(XMAX/N)):
                 sel = np.logical_and(xmin <= df['time'], df['time'] < xmax)
-                Y[i] = sum(df['delay'][sel])
+                Y[i] = sum(df['delay'][sel])/N
+                X[i] = xmin
+                xmin += N
+                xmax += N
         return X,Y
 
 plt.figure()
 for label in labels:
         try:
-                df = pd.read_csv('%s/trace.csv' % (label))
-                X,Y = process(df)
+                df = pd.read_csv('%s/trace.csv' % str(label))
+                X,Y = process(df,label)
                 plt.plot(X,Y,label=label)
         except Exception as e:
                 print(e)
+plt.yscale('log')
 plt.legend()
 plt.savefig(img)
