8d27bb51edf59b6803aadb2977cd6f5651ff069d
Damien Carver <carverdamien@gmail.com>
2018-05-30 12:53:31 +0200

    plot all ratio

--- lab/eval/rr/anon/data/ratio.py
+++ lab/eval/rr/anon/data/ratio.py
@@ -12,11 +12,11 @@ df = pd.read_csv('memory_stats.csv')
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-for label in ['filebench']:
-	sel = df['com.docker.compose.service'] == label
+for label in ['stats.recent_ratio_total', 'stats.recent_ratio_anon', 'stats.recent_ratio_file']:
+	sel = df['com.docker.compose.service'] == 'filebench'
 	X = df['time'][sel]
 	X = np.array(X, dtype='datetime64[ns]')
-	Y = df['stats.recent_ratio_total'][sel]
+	Y = df[label][sel]
 	ax.plot(X,Y,label=label)
 ax.legend()
 ax.set_yscale('log')
