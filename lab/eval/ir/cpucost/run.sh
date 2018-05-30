0538644acfde3dabf22928a83ab213cd90382f91
Damien Carver <carverdamien@gmail.com>
2018-05-30 09:00:12 +0200

    start stop ftrace

--- lab/eval/ir/cpucost/run.sh
+++ lab/eval/ir/cpucost/run.sh
@@ -27,11 +27,19 @@ ${PRE} down
 ${RUN} create
 ${RUN} up -d
 
+# Start ftrace
+${RUN} exec host rm -f /data/${IDLEMEMSTAT_DELAY}/trace.dat*
+${RUN} exec -T host job trace-cmd record -p function_graph -l page_idle_bitmap_read -l page_idle_bitmap_write -g page_idle_bitmap_read -g page_idle_bitmap_write -o /data/${IDLEMEMSTAT_DELAY}/trace.dat
+
 RUN() { ${RUN} exec -T filebench python benchmark.py -- filebench -f workloads/run.f;}
 
 RUN &
 
 wait
+
+# Stop ftrace
+${RUN} exec host bash -c 'kill -s SIGINT $(pgrep trace-cmd)'
+while ${RUN} exec host pgrep trace-cmd; do echo 'waiting'; sleep 1; done
 
 # Report
 mkdir -p data/${IDLEMEMSTAT_DELAY}
