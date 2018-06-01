2c6b9c7eb9bca004f20fe96967b0ffa5ba6203ae
Damien Carver <carverdamien@gmail.com>
2018-06-01 09:33:00 +0200

    results are not as clean with files

--- lab/eval/ir/wss/workloads/prepare.sh
+++ lab/eval/ir/wss/workloads/prepare.sh
@@ -1,24 +1,18 @@
 echo '
-set $dir=/data/A/
-set $filesize=1g
-set $iosize=1m
 
-set $GB=1073741824
-set $MB=1048576
-set $KB=1024
-
-define file name=largefile,path=$dir,size=$filesize,prealloc,reuse
+set $cold=200m
+set $hot=800m
 
 define process name=filereader,instances=1
 {
-  thread name=cold,memsize=$iosize,instances=1
+  thread name=cold,memsize=$cold,instances=1
   {
-    flowop read name=seqread,filename=largefile,iosize=$iosize,random,workingset=1g
+    flowop hog name=cold,value=262144,workingset=$cold,iosize=4
   }
-  thread name=hot,memsize=512m,instances=1
+  thread name=hot,memsize=$hot,instances=1
   {
     flowop eventlimit name=limit
-    flowop hog name=hot,value=262144,workingset=512m,iosize=4
+    flowop hog name=hot,value=262144,workingset=$hot,iosize=4
   }
 }
 
