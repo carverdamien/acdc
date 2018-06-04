d0bad16c9fdafb0c140cf87a21f18d8351dd7e8b
Damien Carver <carverdamien@gmail.com>
2018-06-04 10:04:36 +0200

    compose, host, ftrace

--- lab/eval/ir/wss/run.sh
+++ lab/eval/ir/wss/run.sh
@@ -4,9 +4,12 @@ set -x -e
 source kernel
 [ -n "${KERNEL}" ]
 [ "$(uname -sr)" == "Linux ${KERNEL}" ]
+[ -n "${IDLEMEMSTAT_DELAY}" ]
 
 PRE="docker-compose --project-directory $PWD -f compose/unrestricted.yml"
 RUN="docker-compose --project-directory $PWD -f compose/restricted.yml"
+
+echo 'IDLEMEMSTAT_DELAY=${IDLEMEMSTAT_DELAY}' > .env
 
 # Prepare
 make -C workloads
@@ -24,15 +27,25 @@ ${PRE} down
 ${RUN} create
 ${RUN} up -d
 
+DATA_DIR=data/${IDLEMEMSTAT_DELAY}
+mkdir -p ${DATA_DIR}
+
+# Start ftrace
+${RUN} exec host rm -f /${DATA_DIR}/trace.dat*
+${RUN} exec -T host job trace-cmd record -p function_graph -l page_idle_bitmap_read -l page_idle_bitmap_write -g page_idle_bitmap_read -g page_idle_bitmap_write -o /${DATA_DIR}/trace.dat
+
 RUN() { ${RUN} exec -T filebench python benchmark.py -- filebench -f workloads/run.f;}
 
 RUN &
 
 wait
 
+# Stop ftrace
+${RUN} exec host bash -c 'kill -s SIGINT $(pgrep trace-cmd)'
+while ${RUN} exec host pgrep trace-cmd; do echo 'waiting'; sleep 1; done
+
 # Report
-mkdir -p data
-for m in memory_stats blkio_stats networks cpu_stats filebench_stats
+for m in  $(${PRE} exec influxdb influx -database acdc -execute 'show measurements' -format=csv |  sed 's/\r//g' | tail -n+2 | cut -d, -f2)
 do
-	${PRE} exec influxdb influx -database acdc -execute "select * from $m" -format=csv > data/$m.csv
+	${PRE} exec influxdb influx -database acdc -execute "select * from $m" -format=csv > ${DATA_DIR}/$m.csv
 done
