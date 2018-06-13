b8bf652a396bde2305832d2537a73a1fa814e6bb
Damien Carver <carverdamien@gmail.com>
2018-06-13 17:30:51 +0200

    workloads: use seperate files for rand and seq

--- lab/eval/filebench/workloads/c/prepare.sh
+++ lab/eval/filebench/workloads/c/prepare.sh
@@ -1,9 +1,11 @@
 echo '
 set $dir=/data/C/
-set $filesize=2g
+set $smallfilesize=1g
+set $largefilesize=2g
 set $iosize=1m
 
-define file name=largefile,path=$dir,size=$filesize,prealloc,reuse
+define file name=largefile,path=$dir,size=$largefilesize,prealloc,reuse
+define file name=smallfile,path=$dir,size=$smallfilesize,prealloc,reuse
 
 define process name=filereader,instances=1
 {
@@ -14,7 +16,7 @@ define process name=filereader,instances=1
   thread name=hot,memsize=$iosize,instances=10
   {
     flowop eventlimit name=limit
-    flowop read name=randread,filename=largefile,iosize=$iosize,random,workingset=1g
+    flowop read name=randread,filename=smallfile,iosize=$iosize,random
   }
 }
 
