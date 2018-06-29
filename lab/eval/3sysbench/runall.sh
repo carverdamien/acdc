bb706fc44e6d2310bf996de87da03e382bbe4b45
Damien Carver <carverdamien@gmail.com>
2018-06-29 14:07:22 +0200

    runall.sh: configs from file

--- lab/eval/3sysbench/runall.sh
+++ lab/eval/3sysbench/runall.sh
@@ -1,5 +1,5 @@
 export CONFIG
-for CONFIG in opt nop dc acdc ir-0.01 ir-0.1 rr-0.01 rr-0.1 rr-0.02 rr-0.03 rr-0.04 rr-0.05 rr-0.06 rr-0.07 rr-0.08
+for CONFIG in $(cat configs)
 do
     if [ -d data/$CONFIG ]
     then
