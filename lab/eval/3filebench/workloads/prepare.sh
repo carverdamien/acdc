1da61726ea55fab94d273d6769332f997458a67b
Damien Carver <carverdamien@gmail.com>
2018-07-09 10:36:00 +0200

    single thread

--- lab/eval/3filebench/workloads/prepare.sh
+++ lab/eval/3filebench/workloads/prepare.sh
@@ -13,7 +13,7 @@ define process name=filereader,instances=1
   {
     flowop read name=seqread,filename=largefile,iosize=\$iosize
   }
-  thread name=hot,memsize=\$iosize,instances=10
+  thread name=hot,memsize=\$iosize,instances=1
   {
     flowop read name=randread,filename=smallfile,iosize=\$iosize,random
   }
