ed061f71d439758eddc34c0c69d30c14c94dca29
Damien Carver <carverdamien@gmail.com>
2018-07-09 12:34:10 +0200

    cp -a 3memtier acdcvsdc

--- lab/eval/acdcvsdc/run.sh
+++ lab/eval/acdcvsdc/run.sh
@@ -7,9 +7,19 @@ source kernel
 [ -n "$KERNEL" ]
 [ "$(uname -sr)" == "Linux ${KERNEL}" ]
 
-: ${TOTSEC:=300}
-: ${MEM:=$((2**30))}
-: ${MEMORY:=$((MEM + 512*2**20))}
+: ${SCALE:=16}
+: ${MEM:=$((2**30/SCALE))}
+: ${MEMORY:=$((2*MEM+10*2**20))}
+
+SIZE=$((2**12)) # 512MB max
+REQUESTS=$((189841/SCALE))
+REQUESTS=$((REQUESTS-1280))
+REQUESTS=$((REQUESTS-256-128))
+
+THREADS=1
+EXTRA_INIT="-d ${SIZE} --key-pattern=S:S --key-maximum=${REQUESTS} --ratio=1:0 --requests=${REQUESTS} -c 1 -t ${THREADS}"
+EXTRA_HIGH="-d ${SIZE} --key-pattern=R:R --key-maximum=${REQUESTS} --ratio=0:1 -c 1 -t ${THREADS}"
+EXTRA_LOW="-d ${SIZE} --key-pattern=R:R --key-maximum=$((REQUESTS * 40 / 100)) --ratio=0:1 -c 1 -t ${THREADS}"
 
 SCANNER_CPU_LIMIT=1
 IDLEMEMSTAT_CPU_LIMIT=1
@@ -17,8 +27,15 @@ IDLEMEMSTAT_CPU_LIMIT=1
 once_prelude() { :; }
 prelude() { :; }
 
-activated() { :; }
-deactivated() { :; }
+slow_cpu() { docker update --cpus 0.01 $1; }
+slow_cpu() { echo 1000 | sudo tee "/sys/fs/cgroup/cpu/parent/$1/cpu.cfs_quota_us"; echo 1000000 | sudo tee "/sys/fs/cgroup/cpu/parent/$1/cpu.cfs_period_us"; }
+
+fast_cpu() { docker update --cpus 8 $1; }
+fast_cpu() { echo 8000000 | sudo tee "/sys/fs/cgroup/cpu/parent/$1/cpu.cfs_quota_us"; echo 1000000 | sudo tee "/sys/fs/cgroup/cpu/parent/$1/cpu.cfs_period_us"; }
+
+activate()   { fast_cpu $1; }
+deactivate() { slow_cpu $1; }
+
 
 case "$CONFIG" in
     *opt)
@@ -27,26 +44,22 @@ case "$CONFIG" in
     *nop)
 	;;
 	*orcl)
-	activated()   { echo -1 | sudo tee "/sys/fs/cgroup/memory/parent/$1/memory.soft_limit_in_bytes"; }
-	deactivated() { echo  0 | sudo tee "/sys/fs/cgroup/memory/parent/$1/memory.soft_limit_in_bytes"; }
-	exit 1
+	activate()   { echo -1 | sudo tee "/sys/fs/cgroup/memory/parent/$1/memory.soft_limit_in_bytes"; fast_cpu $1; }
+	deactivate() { echo  0 | sudo tee "/sys/fs/cgroup/memory/parent/$1/memory.soft_limit_in_bytes"; slow_cpu $1; }
 	;;
     *rr-*.*)
 	SCANNER_CPU_LIMIT=${CONFIG##*rr-}
 	once_prelude() { ${RUN} exec scanner job reclaimordersetter /rootfs/sys/fs/cgroup/memory/parent $((2**20)) 0; }
-	exit 1
 	;;
     *ir-*.*)
 	IDLEMEMSTAT_CPU_LIMIT=${CONFIG##*ir-}
 	# once_prelude() { ${RUN} exec idlememstat job idlememstat -d 0 --influxdbhost influxdb --influxdbname=acdc --cgroup /rootfs/sys/fs/cgroup/memory/parent --updateSoftLimit; }
 	once_prelude() { ${RUN} exec idlememstat job idlememstat -d 0 --influxdbhost influxdb --influxdbname=acdc --cgroup /rootfs/sys/fs/cgroup/memory/parent --updateReclaimOrder; }
-	exit 1
 	;;
     *ir-*)
 	IDLEMEMSTAT_DELAY=${CONFIG##*ir-}
 	# once_prelude() { ${RUN} exec idlememstat job idlememstat -d ${IDLEMEMSTAT_DELAY} --influxdbhost influxdb --influxdbname=acdc --cgroup /rootfs/sys/fs/cgroup/memory/parent --updateSoftLimit; }
 	once_prelude() { ${RUN} exec idlememstat job idlememstat -d ${IDLEMEMSTAT_DELAY} --influxdbhost influxdb --influxdbname=acdc --cgroup /rootfs/sys/fs/cgroup/memory/parent --updateReclaimOrder; }
-	exit 1
 	;;
     *dc)
 	prelude() { ${RUN} exec host bash -c "echo 1 | tee /rootfs/sys/fs/cgroup/memory/parent/$1/memory.use_clock_demand"; }
@@ -68,18 +81,20 @@ sed "s/\${SCANNER_CPU_LIMIT}/${SCANNER_CPU_LIMIT}/" > compose/.restricted.yml
 # Prepare
 DATA_DIR="data/$CONFIG/"
 mkdir -p "$DATA_DIR"
-make -B -C workloads MEM=${MEM} TOTSEC=${TOTSEC}
+
 ${RUN} down --remove-orphans
 ${PRE} down --remove-orphans
 ${PRE} build
 ${PRE} create
 ${PRE} up -d
-${PRE} exec filebencha filebench -f workloads/filebencha/prepare.f
-${PRE} exec filebenchb filebench -f workloads/filebenchb/prepare.f
-${PRE} exec filebenchc filebench -f workloads/filebenchc/prepare.f
+${PRE} exec memtiera run -- memtier_benchmark -s redisa ${EXTRA_INIT}
+${PRE} exec redisa redis-cli save
+${PRE} exec memtierb run -- memtier_benchmark -s redisb ${EXTRA_INIT}
+${PRE} exec redisb redis-cli save
+${PRE} exec memtierc run -- memtier_benchmark -s redisc ${EXTRA_INIT}
+${PRE} exec redisc redis-cli save
 ${PRE} exec host bash -c 'echo 3 > /rootfs/proc/sys/vm/drop_caches'
 ${PRE} exec host bash -c 'echo cfq > /sys/block/sda/queue/scheduler'
-${PRE} exec host bash -c 'echo cfq > /sys/block/sdb/queue/scheduler'
 ${PRE} exec host bash -c '! [ -d /rootfs/sys/fs/cgroup/memory/parent ] || rmdir /rootfs/sys/fs/cgroup/memory/parent'
 ${PRE} exec host bash -c 'mkdir /rootfs/sys/fs/cgroup/memory/parent'
 ${PRE} exec host bash -c 'echo 1 > /rootfs/sys/fs/cgroup/memory/parent/memory.use_hierarchy'
@@ -90,40 +105,55 @@ ${PRE} down
 ${RUN} create
 ${RUN} up -d
 
-for c in filebencha filebenchb filebenchc
+for c in redisa redisb redisc
 do
     prelude $(${RUN} ps -q $c)
 done
 
-once_prelude $(for c in filebencha filebenchb filebenchc; do ${RUN} ps -q $c; done)
+once_prelude $(for c in redisa redisb redisc; do ${RUN} ps -q $c; done)
 
-WARM_A () {
-    ${RUN} exec -T filebencha job tar czf - /data/smallfile > /dev/null;
-    ${RUN} exec -T filebencha job tar czf - /data/smallfile > /dev/null;
-}
+CYCLE=60
+NCYCLE=6
+TOTSEC=$((NCYCLE * CYCLE))
 
-A() { ${RUN} exec -T filebencha job python benchmark.py -- filebench -f workloads/filebencha/reuse.f; }
-B() { ${RUN} exec -T filebenchb job python benchmark.py -- filebench -f workloads/filebenchb/waste.f; }
-C() { ${RUN} exec -T filebenchc job python benchmark.py -- filebench -f workloads/filebenchc/waste.f; }
+A() { ${RUN} exec -T memtiera run -- memtier_benchmark -s redisa ${EXTRA_HIGH} --test-time ${TOTSEC}; }
+B() { ${RUN} exec -T memtierb run -- memtier_benchmark -s redisb ${EXTRA_HIGH} --test-time ${TOTSEC}; }
+C() { ${RUN} exec -T memtierc run -- memtier_benchmark -s redisc ${EXTRA_HIGH} --test-time ${TOTSEC}; }
 
-filebencha() { ${RUN} ps -q filebencha; }
-filebenchb() { ${RUN} ps -q filebenchb; }
-filebenchc() { ${RUN} ps -q filebenchc; }
+redisa() { ${RUN} ps -q redisa; }
+redisb() { ${RUN} ps -q redisb; }
+redisc() { ${RUN} ps -q redisc; }
 
 move_tasks() { for task in $(cat $1/tasks); do echo $task | sudo tee $2/tasks; done; }
-move_tasks() { :; }
+# move_tasks() { :; }
 
-for filebench in filebenchb filebenchc
+for redis in redisb redisc
 do
-    move_tasks "/sys/fs/cgroup/blkio/parent/$($filebench)" "/sys/fs/cgroup/blkio/parent/$(filebencha)"
+    move_tasks "/sys/fs/cgroup/blkio/parent/$($redis)" "/sys/fs/cgroup/blkio/parent/$(redisa)"
 done
 
-# WARM_A
+deactivate $(redisb)
+deactivate $(redisc)
+oldredis=redisc
+
 A | tee a.out &
-sleep 10
 B | tee b.out &
 C | tee c.out &
 
+sleep $CYCLE
+activate $(redisb)
+sleep $CYCLE
+
+for redis in redisa redisb redisc redisa
+do
+    deactivate $($redis)
+    activate   $($oldredis)
+    oldredis=$redis
+    sleep $CYCLE
+done
+
+wait
+wait
 wait
 
 # Report
