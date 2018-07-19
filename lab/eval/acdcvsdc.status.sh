2d70fb2ddd24c691e12bd955f676a452af1ef0d8
Damien Carver <carverdamien@gmail.com>
2018-07-19 17:13:56 +0200

    fix acdcvsdc.status.sh

--- lab/eval/acdcvsdc.status.sh
+++ lab/eval/acdcvsdc.status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 status () 
 { 
-tr '\0' '\n' < /proc/$(ps -x | grep docker-compose | head -n 1 | cut -d ' ' -f 1)/environ | grep --color=auto -E 'CONFIG|PWD';
+tr '\0' '\n' < "/proc/$(pgrep docker-compose | head -n 1 | cut -d ' ' -f 1)/environ" | grep --color=auto -E 'CONFIG|PWD';
 }
 status
