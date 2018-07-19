600546e3cc9699a2a580cba46dcc14da13e0a8df
Damien Carver <carverdamien@gmail.com>
2018-07-19 17:49:58 +0200

    rm unused

--- lab/eval/3memtier/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x -e
-
-source kernel
-[ -n "${KERNEL}" ]
-
-docker-compose -p "${KERNEL}" --project-directory "${PWD}" -f compose/kernelcompile.yml build
-docker-compose -p "${KERNEL}" --project-directory "${PWD}" -f compose/kernelcompile.yml up
--- lab/eval/3memtier/scripts/reboot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x -e
-
-source kernel
-[ -n "${KERNEL}" ]
-
-PATH="${PATH}:${PWD}/utils/grub-list"
-grub-reboot-on "${KERNEL}"
