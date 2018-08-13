66ebb9675b7fb032e5125d320adf0b0e9b126f59
Damien Carver <carverdamien@gmail.com>
2018-08-13 11:26:45 +0200

    add unf to daemon.sh

--- lab/eval/daemon.sh
+++ lab/eval/daemon.sh
@@ -3,7 +3,7 @@ export CONFIG
 until docker info; do echo 'Waiting for docker daemon to start'; sleep 1; done
 for i in {0..99}
 do
-    for c in opt orcl nop dc acdc sacdc ir-0.1 ir-1.0 opt-ir-0.1 opt-ir-1.0 rr-0.01 rrs-0.1 rrs-1.0 opt-rrs-0.1 opt-rrs-1.0
+    for c in opt orcl unf nop dc acdc sacdc ir-0.1 ir-1.0 opt-ir-0.1 opt-ir-1.0 rr-0.01 rrs-0.1 rrs-1.0 opt-rrs-0.1 opt-rrs-1.0
     do
 	for d in {om,im}-{wss,rate}
 	do
