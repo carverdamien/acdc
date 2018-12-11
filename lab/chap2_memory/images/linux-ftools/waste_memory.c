79df9fcc8861c4d9ee1910506c44c8688430384e
Damien Carver <carverdamien@gmail.com>
2018-12-11 08:39:24 +0100

    unzip ftools from https://storage.googleapis.com/google-code-archive-source/v2/code.google.com/linux-ftools/source-archive.zip

--- lab/chap2_memory/images/linux-ftools/waste_memory.c
+++ lab/chap2_memory/images/linux-ftools/waste_memory.c
@@ -5,22 +5,24 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <errno.h> 
+#include <string.h> 
 
 /*
  */
-int main() {
-
-    int res;
+int main(int argc, char *argv[]) {
 
     //number of bytes we should attempt to allocate.
-    unsigned int len = 1 * 1024 * 1024 * 1024;
+    size_t len = 1 * 1024 * 1024 * 1024;
 
-    printf( "Allocating %i bytes ...", len );
+    if ( argc == 2 ) 
+        len = atol( argv[1] );
+
+    printf( "Allocating %ld bytes ...", len );
 
     void *buff = malloc( len );
 
     if ( buff == NULL ) {
-       printf( "\nFailed to malloc." );
+       printf( "\nFailed to malloc.\n" );
        exit( EXIT_FAILURE );
     }
 
@@ -30,6 +32,7 @@ int main() {
 
     fflush( stdout );
 
+    printf( "Sleeping to hold memory...\n" );
     sleep( 2147483648 );
 
 }
