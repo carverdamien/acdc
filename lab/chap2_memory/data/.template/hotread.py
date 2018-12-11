12cb3f1f5378377a2ea6e8e4f0cf07bca6cbe19f
Damien Carver <carverdamien@gmail.com>
2018-12-11 10:53:49 +0100

    plot

---
+++ lab/chap2_memory/data/.template/hotread.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pandas as pd
+import matplotlib as mpl
+mpl.use('Agg')
+mpl.colors.ColorConverter.colors['colora'] = (51./255, 102./255, 204./255)
+mpl.colors.ColorConverter.colors['colorb'] = (255./255, 153./255, 0./255)
+mpl.colors.ColorConverter.colors['active'] = (.5,.5,.5)
+mpl.colors.ColorConverter.colors['inactive'] = (.7,.7,.7)
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+
+font = { 'size' : 20,}
+mpl.rc('font', **font)
+OneSec = np.timedelta64(1, 's')
+
+img = sys.argv[1]
+
+basename = os.path.basename(sys.argv[0])
+basename = os.path.splitext(basename)[0]
+config,opname = basename.split('-')
+
+df = pd.read_csv('%s/filebench_stats.csv' % config)
+
+figsize = (6.4*1.6, 4.8)
+fig = plt.figure(figsize=figsize)
+ax = fig.add_subplot(111)
+
+for filename in np.unique(df['filename']):
+	sel = df['filename'] == filename
+	sel = np.logical_and(sel, df['opname'] == opname)
+	X = np.array(df['time'][sel], dtype='datetime64[ns]')
+	Y = df['mb/s'][sel]
+	ax.plot(X,Y,label=filename)
+
+ax.legend()
+fig.savefig(img)
